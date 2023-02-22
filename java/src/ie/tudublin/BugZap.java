b1931e76e4798968863adc5300b6a3602657352f
Imkelis <123948448+Imkelis@users.noreply.github.com>
2023-02-22 15:26:14 +0000

    Added character, now starting to add movement

--- java/src/ie/tudublin/BugZap.java
+++ java/src/ie/tudublin/BugZap.java
@@ -5,31 +5,48 @@ import processing.core.PApplet;
 public class BugZap extends PApplet
 {
 
+	float playerX = 5;
+	float playerY = 5;
+	float playerWidth = 10;
+
 	public void settings()
 	{
-		size(500, 500);
+		size(1000, 800);
 	}
 
 	public void setup() {
 		//colorMode(HSB);
-		background(255,mouseX,0);
+		background(255, 255, 255);
 
-	
-		
 	}
 
-	
+	void drawPlayer(float x, float y, float w){
+
+		fill(0, 0, 0);
+		stroke(50,50,80);
+		rect(500, 400, 15, 50);
+	}
+
+	public void keyPressed()
+	{
+		if (keyCode == LEFT)
+		{
+			System.out.println("Left arrow pressed");
+		}
+		if (keyCode == RIGHT)
+		{
+			System.out.println("Right arrow pressed");
+		}
+		if (key == ' ')
+		{
+			System.out.println("SPACE key pressed");
+		}
+	}
+
 	public void draw()
 	{	
-		fill(500, 200, 0);
-		noStroke();
-		circle(255, 330, 400);
-
-		fill(0, 50, 200);
-		ellipse(255, 255, 500, mouseX);
-
-		
-
+		background(255, 255, 255);
+		drawPlayer(1000,1000,600);
 	}
 
 }
