dae8ff5f3d0c45ea8d0fab07f7960e76e334b7d1
Imkelis <123948448+Imkelis@users.noreply.github.com>
2023-02-22 17:26:49 +0000

    started fist case

--- java/src/ie/tudublin/Audio1.java
+++ java/src/ie/tudublin/Audio1.java
@@ -92,9 +92,18 @@ public class Audio1 extends PApplet
                 }
                 break;
         case 1:
-            background(0);            
-            break;
-
+            
+        background(0);
+        for(int i = 0 ; i < ab.size() ; i ++)
+        {
+            //float c = map(ab.get(i), -1, 1, 0, 255);
+            float c = map(i, 0, ab.size(), 0, 255);
+            stroke(c, 255, 255);
+            float f = ab.get(i) * halfH;
+            line(i, halfH + f, i, halfH - f);                    
+        }
+        break;
+        
         }
         
 
