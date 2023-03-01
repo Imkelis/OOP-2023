21bac684fcc2a82f4e586cd31d7233c906cd4706
Imkelis <123948448+Imkelis@users.noreply.github.com>
2023-03-01 03:17:06 +0000

    circles added that react to music

--- java/src/ie/tudublin/Audio1.java
+++ java/src/ie/tudublin/Audio1.java
@@ -15,15 +15,15 @@ public class Audio1 extends PApplet
 
     int mode = 0;
 
-    float y = 0;
-    float smoothedY = 0;
     float smoothedAmplitude = 0;
 
+    Circle[] circles;
+
     public void keyPressed() {
-		if (key >= '0' && key <= '9') {
-			mode = key - '0';
-		}
-		if (keyCode == ' ') {
+        if (key >= '0' && key <= '9') {
+            mode = key - '0';
+        }
+        if (keyCode == ' ') {
             if (ap.isPlaying()) {
                 ap.pause();
             } else {
@@ -31,43 +31,36 @@ public class Audio1 extends PApplet
                 ap.play();
             }
         }
-	}
+    }
 
     public void settings()
     {
         size(1024, 1000, P3D);
-        //fullScreen(P3D, SPAN);
     }
 
     public void setup()
     {
         minim = new Minim(this);
-        // Uncomment this to use the microphone
-        // ai = minim.getLineIn(Minim.MONO, width, 44100, 16);
-        // ab = ai.mix; 
-
-        // And comment the next two lines out
         ap = minim.loadFile("heroplanet.mp3", 1024);
         ap.play();
         ab = ap.mix;
         colorMode(HSB);
 
-        y = height / 2;
-        smoothedY = y;
-
+        circles = new Circle[50];
+        for (int i = 0; i < circles.length; i++) {
+            float x = random(width);
+            float y = random(height);
+            float size = random(20, 50);
+            circles[i] = new Circle(x, y, size);
+        }
     }
-
-    float off = 0;
 
     public void draw()
     {
-        //background(0);
         float halfH = height / 2;
         float average = 0;
         float sum = 0;
-        off += 1;
         // Calculate sum and average of the samples
-        // Also lerp each element of buffer;
         for(int i = 0 ; i < ab.size() ; i ++)
         {
             sum += abs(ab.get(i));
@@ -75,52 +68,53 @@ public class Audio1 extends PApplet
         average= sum / (float) ab.size();
 
         smoothedAmplitude = lerp(smoothedAmplitude, average, 0.1f);
-        
-        float cx = width / 2;
-        float cy = height / 2;
 
         switch (mode) {
-			case 0:
+            case 0:
                 background(0);
                 for(int i = 0 ; i < ab.size() ; i ++)
                 {
-                    //float c = map(ab.get(i), -1, 1, 0, 255);
                     float c = map(i, 0, ab.size(), 0, 255);
                     stroke(c, 255, 255);
                     float f = ab.get(i) * halfH;
-                    line(i, halfH + f, i, halfH - f);                    
+                    line(i, halfH + f, i, halfH - f);
                 }
                 break;
-        case 1:
-            
-        background(0);
-                for(int i = 0 ; i < ab.size() ; i ++)
-                {
-                    //float c = map(ab.get(i), -1, 1, 0, 255);
-                    float c = map(i, 0, ab.size(), 0, 255);
-                    stroke(c, 255, 255);
-                    float f = ab.get(i) * halfH;
-                    line(i+ 500, halfH + f, f+500, halfH - i);                  
+            case 1:
+                background(0);
+                for (Circle circle : circles) {
+                    circle.update(smoothedAmplitude);
+                    circle.display(smoothedAmplitude);
                 }
-        
-
+                break;
         }
-        
+    }
 
+    class Circle {
+        float x;
+        float y;
+        float size;
+        float angle;
 
-        
-        // Other examples we made in the class
-        /*
-        stroke(255);
-        fill(100, 255, 255);        
-        
-        circle(width / 2, halfH, lerpedA * 100);
+        Circle(float x, float y, float size) {
+            this.x = x;
+            this.y = y;
+            this.size = size;
+            this.angle = random(TWO_PI);
+        }
 
-        circle(100, y, 50);
-        y += random(-10, 10);
-        smoothedY = lerp(smoothedY, y, 0.1f);        
-        circle(200, smoothedY, 50);
-        */
+        void update(float amplitude) {
+            float speed = map(amplitude, 0, 1, 0, 5);
+            angle += speed;
+        }
 
-    }        
+        void display(float amplitude) {
+            float hue = map(amplitude, 0, 1, 0, 255);
+            fill(hue, 255, 255);
+            noStroke();
+            float d = size + map(sin(angle), -1, 1, -10, 10);
+            ellipse(x, y, d, d);
+
+    } 
 }
+}       
